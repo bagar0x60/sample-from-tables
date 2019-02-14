93608c8c64be78431c4698bca349fdce54c63462
bagar0x60 <leha969696@yandex.ru>
2019-02-14 11:23:57 +0300

    more concise code

--- get_sample.py
+++ get_sample.py
@@ -6,24 +6,18 @@ import sys
 
 STATS_TABLE_SIZE = 400
 
-def usage():
-    print(  f'USAGE: get_sample <k>\n'
-            f'<k> - sample size, integer from 0 to {STATS_TABLE_SIZE}')
-    exit()
-
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        usage()
-    
+if __name__ == '__main__':    
     try:
         k = int(sys.argv[1])
         assert 0 <= k <= 400 
     except:
-        usage()    
+        print(  f'USAGE: get_sample <k>\n'
+                f'<k> - sample size, integer from 0 to {STATS_TABLE_SIZE}')
+        exit()
 
     stats_table = []
     with open('stats_tables.csv') as stats_tables_file:
-        reader = csv.reader(stats_tables_file, delimiter=',', quotechar='"')
+        reader = csv.reader(stats_tables_file)
         for row in reader:
             if row[0] == '':
                 continue
